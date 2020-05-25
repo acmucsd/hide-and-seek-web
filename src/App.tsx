06839a04b7b1825123c11430053ec5d155081156
StoneT2000 <stoneztao@gmail.com>
2020-05-25 16:54:31 -0700

    google analytics installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import { UserProvider } from './UserContext'
 import { TournamentProvider } from './contexts/tournament';
 import { Spin } from 'antd';
@@ -7,9 +7,9 @@ import { LoadingOutlined } from '@ant-design/icons';
 import SetupTournament from './containers/tournament';
 
 import './styles/index.scss';
-
+import ReactGA from 'react-ga';
 import MainPage from './pages/MainPage';
-import DimensionPage from './pages/DimensionsPage';
+
 import MatchPage from './pages/MatchPage';
 import TournamentRankingsPage from './pages/TournamentRankingsPage';
 import TournamentPage from './pages/TournamentPage';
@@ -24,12 +24,23 @@ import { verifyToken, getUserFromToken } from './actions/auth';
 import { DIMENSION_ID, COOKIE_NAME, defaultUser, defaultTournament } from './configs';
 import { message } from 'antd';
 
+
 let cookie = getCookie(COOKIE_NAME);
 function App() {
+
   const [user, setUser] = useState(defaultUser);
   const [tournament, setTournament] = useState(defaultTournament);
   const [verifying, setVerifying] =  useState(true);
+  function usePageViews() {
+    let location = useLocation()
+    ReactGA.initialize('UA-167602471-1');
+    useEffect(() => {
+      console.log(location.pathname);
+      ReactGA.pageview(location.pathname);
+    }, [location]);
+  }
   const antIcon = <LoadingOutlined style={{ fontSize: '2rem' }} spin />;
+  
   useEffect(() => {
     if (cookie) {
       // verify cookie
@@ -47,54 +58,53 @@ function App() {
       setVerifying(false);
     }
   }, []);
+  usePageViews();
   return (
-    <Router>
-      <div>
-        <Switch>
-        {!verifying ?
-          <UserProvider value={{user: user, setUser: setUser}}>
-            
-            <Route path="/" exact component={MainPage} />
-            <Route path="/register" exact component={RegisterPage} />
-            <Route path="/login" exact component={LoginPage} />
-            <Route path="/dimensions/:id/matches/:matchID" exact component={MatchPage} />
+    <div>
+      <Switch>
+      {!verifying ?
+        <UserProvider value={{user: user, setUser: setUser}}>
+          
+          <Route path="/" exact component={MainPage} />
+          <Route path="/register" exact component={RegisterPage} />
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/dimensions/:id/matches/:matchID" exact component={MatchPage} />
 
-            <TournamentProvider value={{tournament: tournament, setTournament: setTournament}}>
-              <Route 
-                path="/tournaments/:tournamentID" 
-                exact 
-                render={() => <SetupTournament component={<TournamentPage />} />}
-              />
-              <Route 
-                path="/tournaments/:tournamentID/ranks" 
-                exact 
-                render={() => <SetupTournament component={<TournamentRankingsPage />} />}
-              />
-              <Route 
-                path="/tournaments/:tournamentID/user/:userID" 
-                exact 
-                render={() => <SetupTournament component={<ProfilePage />} />}
-              />
-              <Route 
-                path="/tournaments/:tournamentID/match/:matchID" 
-                exact 
-                render={() => <SetupTournament component={<TournamentMatchPage />} />}
-              />
-              <Route path="/tournaments/:tournamentID/upload" exact 
-                render={() => <SetupTournament component={<UploadBotPage />} />}
-              />
-            </TournamentProvider>
-          </UserProvider> :
-          <div className='Loading' style={{
-            textAlign: 'center',
-            fontSize: '2rem',
-            height: '100vh',
-            lineHeight: '100vh'
-          }}>Loading <Spin indicator={antIcon} /></div>
-        }
-        </Switch>
-      </div>
-    </Router>
+          <TournamentProvider value={{tournament: tournament, setTournament: setTournament}}>
+            <Route 
+              path="/tournaments/:tournamentID" 
+              exact 
+              render={() => <SetupTournament component={<TournamentPage />} />}
+            />
+            <Route 
+              path="/tournaments/:tournamentID/ranks" 
+              exact 
+              render={() => <SetupTournament component={<TournamentRankingsPage />} />}
+            />
+            <Route 
+              path="/tournaments/:tournamentID/user/:userID" 
+              exact 
+              render={() => <SetupTournament component={<ProfilePage />} />}
+            />
+            <Route 
+              path="/tournaments/:tournamentID/match/:matchID" 
+              exact 
+              render={() => <SetupTournament component={<TournamentMatchPage />} />}
+            />
+            <Route path="/tournaments/:tournamentID/upload" exact 
+              render={() => <SetupTournament component={<UploadBotPage />} />}
+            />
+          </TournamentProvider>
+        </UserProvider> :
+        <div className='Loading' style={{
+          textAlign: 'center',
+          fontSize: '2rem',
+          height: '100vh',
+          lineHeight: '100vh'
+        }}>Loading <Spin indicator={antIcon} /></div>
+      }
+      </Switch>
+    </div>
   );
 }
 
